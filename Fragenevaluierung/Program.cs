8d44a5b0c07d7c74aa9db17b1c62f6b456dde5ce
DavidFeirer <david.feirer@edu.campus02.at>
2023-06-18 13:57:14 +0200

    register frageevaluierungservice to local consul agent

--- Fragenevaluierung/Program.cs
+++ Fragenevaluierung/Program.cs
@@ -1,5 +1,11 @@
 using Fragenevaluierung.Service;
 
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +16,32 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ICheckFrage, CheckFrage>();
+
+// Registrierung des Consul-Clients als Singleton
+builder.Services.AddSingleton<IConsulClient>(sp => new ConsulClient(config =>
+{
+    config.Address = new Uri("http://localhost:8500"); // Adresse des lokalen Consul-Agents
+}));
+
+// Registrierung des Microservices bei Consul
+builder.Services.AddSingleton<IHostedService>(sp =>
+{
+    var consulClient = sp.GetRequiredService<IConsulClient>();
+    var serviceName = "mein-microservice";
+    var serviceId = System.Guid.NewGuid().ToString();
+    var servicePort = 8080;
+
+    // Metadaten für den Microservice
+    var registration = new AgentServiceRegistration
+    {
+        ID = serviceId,
+        Name = serviceName,
+        Port = servicePort,
+        Tags = new[] { "tag1", "tag2" }
+    };
+
+    return new ConsulHostedService(registration, consulClient);
+});
 
 var app = builder.Build();
 
---
+++ Fragenevaluierung/Service/ConsulHostedService.cs
@@ -0,0 +1,26 @@
+﻿using Consul;
+
+namespace Fragenevaluierung.Service
+{
+    public class ConsulHostedService : IHostedService
+    {
+        private readonly AgentServiceRegistration _registration;
+        private readonly IConsulClient _consulClient;
+
+        public ConsulHostedService(AgentServiceRegistration registration, IConsulClient consulClient)
+        {
+            _registration = registration;
+            _consulClient = consulClient;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _consulClient.Agent.ServiceRegister(_registration, cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _consulClient.Agent.ServiceDeregister(_registration.ID, cancellationToken);
+        }
+    }
+}
